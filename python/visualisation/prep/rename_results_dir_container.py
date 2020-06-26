c3507a086ce32deecc2927ac68040d05e77e96fa
Andy Wang <aw1912@ic.ac.uk>
2020-06-26 15:46:47 +0100

    adding rename results dir script for hpc run

---
+++ python/visualisation/prep/rename_results_dir_container.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+full_path = sys.argv[1]
+full_path = full_path.split("/")
+result_dir = full_path[-2]
+
+path_to_variant = "/".join(full_path[:-2])
+os.chdir(path_to_variant)
+
+components = result_dir.split("_")
+args = [i.split("=")[-1] for i in components]
+
+new_result_dir_name = f"gen{args[0]}_random{args[1]}_fullloss{args[2]}_beta{args[3]}_extension{args[4]}_lossfunc{args[5]}"
+
+os.rename(result_dir, new_result_dir_name)
