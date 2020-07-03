ac2c262e9f9c5616f41cd86cc7366f652d78d91b
Andy Wang <aw1912@ic.ac.uk>
2020-07-03 11:17:55 +0100

    increasing nb_epochs for smooth L1 run

--- src/params.hpp
+++ src/params.hpp
@@ -32,7 +32,7 @@ struct Params {
 
     struct ae {
     SFERES_CONST size_t batch_size = 256;
-    SFERES_CONST size_t nb_epochs = 6000;
+    SFERES_CONST size_t nb_epochs = 15000;
     SFERES_CONST float learning_rate = 1e-4;
     SFERES_CONST float CV_fraction = 0.80;
 
