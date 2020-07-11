00f814f2c2aeb628ca727cca374414155da1b7b7
Andy Wang <aw1912@ic.ac.uk>
2020-07-11 18:58:50 +0100

    adding mean to ylabel for plot variants

--- python/visualisation/plot_variants.py
+++ python/visualisation/plot_variants.py
@@ -7,12 +7,17 @@ path = "/media/andwang1/SAMSUNG/MSC_INDIV/results_box2d_bsd_exp1"
 os.chdir(path)
 
 plotting_groups = [
-    ["l1", "smoothl1"],
+    ["l1", "smoothl1_longtrain"],
     ["l1", "l2"],
     ["extension03", "l2"],
     ["l1extension03", "l1"],
+    ["l1beta0extension03", "l1beta0"],
     ["l2beta0", "l2"],
-    ["l1beta0", "l1"]
+    ["l1beta0", "l1"],
+    ["smoothl1_longtrain", "smoothl1_shorttrain"],
+    ["l1nosample", "l1"],
+    ["randomsolutions", "l1"],
+    ["randomsolutions", "l1nosample"],
 ]
 
 colours = ["blue", "brown", "grey", "red", "purple", "green", "pink"]
@@ -44,7 +49,7 @@ for group in plotting_groups:
                 ax1.lines[-1].set_linestyle("--")
             colour_count += 1
     ax1.set_title("Losses - Actual L2")
-    ax1.set_ylabel("L2")
+    ax1.set_ylabel("Mean L2")
     ax1.set_xlabel("Stochasticity")
     plt.savefig(f"{save_dir}/losses_{'_'.join(group)}.png")
     plt.close()
@@ -63,7 +68,7 @@ for group in plotting_groups:
                 ax1.lines[-1].set_linestyle("--")
             colour_count += 1
     ax1.set_title("Diversity Score")
-    ax1.set_ylabel("Diversity")
+    ax1.set_ylabel("Mean Diversity")
     ax1.set_xlabel("Stochasticity")
     plt.savefig(f"{save_dir}/diversity_{'_'.join(group)}.png")
     plt.close()
@@ -83,7 +88,7 @@ for group in plotting_groups:
                 ax1.lines[-1].set_linestyle("--")
             colour_count += 1
     ax1.set_title("% Solutions Moving The Ball")
-    ax1.set_ylabel("%")
+    ax1.set_ylabel("Mean %")
     ax1.set_xlabel("Stochasticity")
     plt.savefig(f"{save_dir}/pct_moved_{'_'.join(group)}.png")
     plt.close()
@@ -103,7 +108,7 @@ for group in plotting_groups:
                 ax1.lines[-1].set_linestyle("--")
             colour_count += 1
     ax1.set_title("Mean Distance Moved Excl. No-Move")
-    ax1.set_ylabel("Distance")
+    ax1.set_ylabel("Mean Distance")
     ax1.set_xlabel("Stochasticity")
     plt.savefig(f"{save_dir}/dist_{'_'.join(group)}.png")
     plt.close()
@@ -123,7 +128,7 @@ for group in plotting_groups:
                 ax1.lines[-1].set_linestyle("--")
             colour_count += 1
     ax1.set_title("Mean Variance of Distance Moved Excl. No-Move")
-    ax1.set_ylabel("Variance")
+    ax1.set_ylabel("Mean Variance")
     ax1.set_xlabel("Stochasticity")
     plt.savefig(f"{save_dir}/dist_var_{'_'.join(group)}.png")
     plt.close()
@@ -143,7 +148,7 @@ for group in plotting_groups:
                 ax1.lines[-1].set_linestyle("--")
             colour_count += 1
     ax1.set_title("Entropy of Trajectory Positions Excl. No-Move")
-    ax1.set_ylabel("Mean Entropy")
+    ax1.set_ylabel("Mean Mean Entropy")
     ax1.set_xlabel("Stochasticity")
     plt.savefig(f"{save_dir}/entropy_{'_'.join(group)}.png")
     plt.close()
@@ -163,7 +168,7 @@ for group in plotting_groups:
                 ax1.lines[-1].set_linestyle("--")
             colour_count += 1
     ax1.set_title("Variance of Trajectory Positions")
-    ax1.set_ylabel("Mean Variance")
+    ax1.set_ylabel("Mean Mean Variance")
     ax1.set_xlabel("Stochasticity")
     plt.savefig(f"{save_dir}/posvar_{'_'.join(group)}.png")
     plt.close()
@@ -185,7 +190,7 @@ for group in plotting_groups:
                 ax1.lines[-1].set_linestyle("--")
             colour_count += 1
     ax1.set_title("Reconstruction Var. of No-Move solutions")
-    ax1.set_ylabel("Mean Reconstruction Variance")
+    ax1.set_ylabel("Mean Mean Reconstruction Variance")
     ax1.set_xlabel("Stochasticity")
     plt.savefig(f"{save_dir}/recon_var_{'_'.join(group)}.png")
     plt.close()
@@ -206,30 +211,30 @@ for group in plotting_groups:
             if i == 0:
                 ax1.lines[-1].set_linestyle("--")
             colour_count += 1
-    ax1.set_ylabel("Mean Variance")
+    ax1.set_ylabel("Mean Mean Variance")
     ax1.set_xlabel("Stochasticity")
     ax1.set_title(f"Variance of Latent Descriptors of No-Move Solutions")
     plt.savefig(f"{save_dir}/latent_var_{'_'.join(group)}.png")
     plt.close()
 
-    f = plt.figure(figsize=(20, 20))
-    spec = f.add_gridspec(1, 2)
-    ax1 = f.add_subplot(spec[0, :])
-    colour_count = 0
-    for i, member in enumerate(group):
-        with open(f"{member}/loss_data.pk", "rb") as f:
-            log_data = pk.load(f)
-
-        for variant, data in log_data.items():
-            if "vae" not in variant:
-                continue
-            sns.lineplot(data["stoch"], data["ENVAR"], estimator="mean", ci="sd", label=f"{member}-{variant}", ax=ax1,
-                         color=colours[colour_count])
-            if i == 0:
-                ax1.lines[-1].set_linestyle("--")
-            colour_count += 1
-    ax1.set_ylabel("Mean Variance")
-    ax1.set_xlabel("Stochasticity")
-    ax1.set_title(f"Encoder Variance")
-    plt.savefig(f"{save_dir}/encoder_var_{'_'.join(group)}.png")
-    plt.close()
+    # f = plt.figure(figsize=(20, 20))
+    # spec = f.add_gridspec(1, 2)
+    # ax1 = f.add_subplot(spec[0, :])
+    # colour_count = 0
+    # for i, member in enumerate(group):
+    #     with open(f"{member}/loss_data.pk", "rb") as f:
+    #         log_data = pk.load(f)
+    #
+    #     for variant, data in log_data.items():
+    #         if "vae" not in variant:
+    #             continue
+    #         sns.lineplot(data["stoch"], data["ENVAR"], estimator="mean", ci="sd", label=f"{member}-{variant}", ax=ax1,
+    #                      color=colours[colour_count])
+    #         if i == 0:
+    #             ax1.lines[-1].set_linestyle("--")
+    #         colour_count += 1
+    # ax1.set_ylabel("Mean Mean Variance")
+    # ax1.set_xlabel("Stochasticity")
+    # ax1.set_title(f"Encoder Variance")
+    # plt.savefig(f"{save_dir}/encoder_var_{'_'.join(group)}.png")
+    # plt.close()
