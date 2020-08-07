0e02934eda54b5fdbaa22ddbdb717698c10b8866
Andy Wang <aw1912@ic.ac.uk>
2020-08-07 17:47:25 +0100

    updating plotvariants to median and interquartiles

--- python/visualisation/plot_variants.py
+++ python/visualisation/plot_variants.py
@@ -2,14 +2,19 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 import pickle as pk
+import numpy as np
+import pandas as pd
 
 path = "/media/andwang1/SAMSUNG/MSC_INDIV/results_box2d_bsd_exp1"
 os.chdir(path)
 
 plotting_groups = [
-["l2beta0nosample", "l2beta0nosampletrain"],
+    # ["tsne_nosampletrain_beta0", "tsne_nosampletrain"],
+# ["tsne_nosampletrain_beta0", "sne_sampletrain", "sne_nosampletrain", "tsne_nosampletrain"],
 # ["l1beta0nosample","l1nosample", "l1beta0"],
 # ["l2beta0nosample","l2nosample", "l2beta0"],
+#     ["sne_nosampletrain_beta0", "sne_nosampletrain"]
+["l2nosampletrain", "sne_nosampletrain", "tsne_nosampletrain"]
 # ["sne_nosampletrain_beta1", "tsne_nosampletrain_beta1", "l2beta1nosampletrain"],
 # ["l2beta0nosampletrain", "l2beta1nosampletrain"],
 # ["l2beta1nosampletrain", "l2"], #compare against ae
@@ -35,7 +40,9 @@ plotting_groups = [
 # ["l2beta1nosampletrain", "l2nosample"],
 ]
 
-colours = ["blue", "brown", "grey", "red", "purple", "green", "pink", "orange"]
+# f"{member}-{variant_name}" if not (member == "l2beta0nosampletrain" and variant_name == "vaefulllossfalse") else f"{member}-{variant_name} / l2-ae"
+
+colours = ["blue", "brown", "grey", "green", "purple", "red", "pink", "orange"]
 
 # make legend bigger
 plt.rc('legend', fontsize=35)
@@ -58,15 +65,19 @@ for group in plotting_groups:
             log_data = pk.load(f)
 
         for variant, data in log_data.items():
-            if "aurora" in variant or variant.startswith("ae"):
+            if "aurora" in variant :#or variant.startswith("ae"):
                 continue
             variant_name = variant if not variant.startswith("ae") else "ae"
-            sns.lineplot(data["stoch"], data["AL"], estimator="mean", ci="sd", label=f"{member}-{variant_name}", ax=ax1, color=colours[colour_count])
+            sns.lineplot(data["stoch"], data["AL"], estimator=np.median, ci=None, label=f"{member}-{variant_name}", ax=ax1, color=colours[colour_count])
             if i == 0 and len(group) > 1:
                 ax1.lines[-1].set_linestyle("--")
+            data_stats = pd.DataFrame(data)[["stoch", "AL"]].groupby("stoch").describe()
+            quart25 = data_stats[('AL', '25%')]
+            quart75 = data_stats[('AL', '75%')]
+            ax1.fill_between([0, 1, 2, 3, 4, 5], quart25, quart75, alpha=0.3, color=colours[colour_count])
             colour_count += 1
     ax1.set_title("Losses - Actual L2")
-    ax1.set_ylabel("Mean L2")
+    ax1.set_ylabel("L2")
     ax1.set_xlabel("Stochasticity")
     plt.savefig(f"{save_dir}/pdf/losses_{'_'.join(group)}.pdf")
     plt.savefig(f"{save_dir}/losses_{'_'.join(group)}.png")
@@ -81,15 +92,19 @@ for group in plotting_groups:
             log_data = pk.load(f)
 
         for variant, data in log_data.items():
-            if "aurora" in variant or variant.startswith("ae"):
+            if "aurora" in variant :#or variant.startswith("ae"):
                 continue
             variant_name = variant if not variant.startswith("ae") else "ae"
-            sns.lineplot(data["stoch"], data["DIV"], estimator="mean", ci="sd", label=f"{member}-{variant_name}", ax=ax1, color=colours[colour_count])
+            sns.lineplot(data["stoch"], data["DIV"], estimator=np.median, ci=None, label=f"{member}-{variant_name}", ax=ax1, color=colours[colour_count])
+            data_stats = pd.DataFrame(data)[["stoch", "DIV"]].groupby("stoch").describe()
+            quart25 = data_stats[('DIV', '25%')]
+            quart75 = data_stats[('DIV', '75%')]
+            ax1.fill_between([0, 1, 2, 3, 4, 5], quart25, quart75, alpha=0.3, color=colours[colour_count])
             if i == 0 and len(group) > 1:
                 ax1.lines[-1].set_linestyle("--")
             colour_count += 1
     ax1.set_title("Diversity Score")
-    ax1.set_ylabel("Mean Diversity")
+    ax1.set_ylabel("Diversity")
     ax1.set_xlabel("Stochasticity")
     plt.savefig(f"{save_dir}/pdf/diversity_{'_'.join(group)}.pdf")
     plt.savefig(f"{save_dir}/diversity_{'_'.join(group)}.png")
@@ -104,16 +119,20 @@ for group in plotting_groups:
             log_data = pk.load(f)
 
         for variant, data in log_data.items():
-            if "aurora" in variant or variant.startswith("ae"):
+            if "aurora" in variant :#or variant.startswith("ae"):
                 continue
             variant_name = variant if not variant.startswith("ae") else "ae"
-            sns.lineplot(data["stoch"], data["PCT"], estimator="mean", ci="sd", label=f"{member}-{variant_name}", ax=ax1,
+            sns.lineplot(data["stoch"], data["PCT"], estimator=np.median, ci=None, label=f"{member}-{variant_name}", ax=ax1,
                          color=colours[colour_count])
+            data_stats = pd.DataFrame(data)[["stoch", "PCT"]].groupby("stoch").describe()
+            quart25 = data_stats[('PCT', '25%')]
+            quart75 = data_stats[('PCT', '75%')]
+            ax1.fill_between([0, 1, 2, 3, 4, 5], quart25, quart75, alpha=0.3, color=colours[colour_count])
             if i == 0 and len(group) > 1:
                 ax1.lines[-1].set_linestyle("--")
             colour_count += 1
     ax1.set_title("% Solutions Moving The Ball")
-    ax1.set_ylabel("Mean %")
+    ax1.set_ylabel("%")
     ax1.set_xlabel("Stochasticity")
     plt.savefig(f"{save_dir}/pdf/pct_moved_{'_'.join(group)}.pdf")
     plt.savefig(f"{save_dir}/pct_moved_{'_'.join(group)}.png")
@@ -128,16 +147,20 @@ for group in plotting_groups:
             log_data = pk.load(f)
 
         for variant, data in log_data.items():
-            if "aurora" in variant or variant.startswith("ae"):
+            if "aurora" in variant :#or variant.startswith("ae"):
                 continue
             variant_name = variant if not variant.startswith("ae") else "ae"
-            sns.lineplot(data["stoch"], data["MDE"], estimator="mean", ci="sd", label=f"{member}-{variant_name}", ax=ax1,
+            sns.lineplot(data["stoch"], data["MDE"], estimator=np.median, ci=None, label=f"{member}-{variant_name}", ax=ax1,
                          color=colours[colour_count])
+            data_stats = pd.DataFrame(data)[["stoch", "MDE"]].groupby("stoch").describe()
+            quart25 = data_stats[('MDE', '25%')]
+            quart75 = data_stats[('MDE', '75%')]
+            ax1.fill_between([0, 1, 2, 3, 4, 5], quart25, quart75, alpha=0.3, color=colours[colour_count])
             if i == 0 and len(group) > 1:
                 ax1.lines[-1].set_linestyle("--")
             colour_count += 1
-    ax1.set_title("Mean Distance Moved Excl. No-Move")
-    ax1.set_ylabel("Mean Distance")
+    ax1.set_title("Distance Moved Excl. No-Move")
+    ax1.set_ylabel("Distance")
     ax1.set_xlabel("Stochasticity")
     plt.savefig(f"{save_dir}/pdf/dist_{'_'.join(group)}.pdf")
     plt.savefig(f"{save_dir}/dist_{'_'.join(group)}.png")
@@ -152,16 +175,20 @@ for group in plotting_groups:
             log_data = pk.load(f)
 
         for variant, data in log_data.items():
-            if "aurora" in variant or variant.startswith("ae"):
+            if "aurora" in variant :#or variant.startswith("ae"):
                 continue
             variant_name = variant if not variant.startswith("ae") else "ae"
-            sns.lineplot(data["stoch"], data["VDE"], estimator="mean", ci="sd", label=f"{member}-{variant_name}", ax=ax1,
+            sns.lineplot(data["stoch"], data["VDE"], estimator=np.median, ci=None, label=f"{member}-{variant_name}", ax=ax1,
                          color=colours[colour_count])
+            data_stats = pd.DataFrame(data)[["stoch", "VDE"]].groupby("stoch").describe()
+            quart25 = data_stats[('VDE', '25%')]
+            quart75 = data_stats[('VDE', '75%')]
+            ax1.fill_between([0, 1, 2, 3, 4, 5], quart25, quart75, alpha=0.3, color=colours[colour_count])
             if i == 0 and len(group) > 1:
                 ax1.lines[-1].set_linestyle("--")
             colour_count += 1
-    ax1.set_title("Mean Variance of Distance Moved Excl. No-Move")
-    ax1.set_ylabel("Mean Variance")
+    ax1.set_title("Variance of Distance Moved Excl. No-Move")
+    ax1.set_ylabel("Variance")
     ax1.set_xlabel("Stochasticity")
     plt.savefig(f"{save_dir}/pdf/dist_var_{'_'.join(group)}.pdf")
     plt.savefig(f"{save_dir}/dist_var_{'_'.join(group)}.png")
@@ -176,16 +203,20 @@ for group in plotting_groups:
             log_data = pk.load(f)
 
         for variant, data in log_data.items():
-            if "aurora" in variant or variant.startswith("ae"):
+            if "aurora" in variant :#or variant.startswith("ae"):
                 continue
             variant_name = variant if not variant.startswith("ae") else "ae"
-            sns.lineplot(data["stoch"], data["EVE"], estimator="mean", ci="sd", label=f"{member}-{variant_name}", ax=ax1,
+            sns.lineplot(data["stoch"], data["EVE"], estimator=np.median, ci=None, label=f"{member}-{variant_name}", ax=ax1,
                          color=colours[colour_count])
+            data_stats = pd.DataFrame(data)[["stoch", "EVE"]].groupby("stoch").describe()
+            quart25 = data_stats[('EVE', '25%')]
+            quart75 = data_stats[('EVE', '75%')]
+            ax1.fill_between([0, 1, 2, 3, 4, 5], quart25, quart75, alpha=0.3, color=colours[colour_count])
             if i == 0 and len(group) > 1:
                 ax1.lines[-1].set_linestyle("--")
             colour_count += 1
     ax1.set_title("Entropy of Trajectory Positions Excl. No-Move")
-    ax1.set_ylabel("Mean Entropy")
+    ax1.set_ylabel("Entropy")
     ax1.set_xlabel("Stochasticity")
     plt.savefig(f"{save_dir}/pdf/entropy_{'_'.join(group)}.pdf")
     plt.savefig(f"{save_dir}/entropy_{'_'.join(group)}.png")
@@ -200,16 +231,21 @@ for group in plotting_groups:
             log_data = pk.load(f)
 
         for variant, data in log_data.items():
-            if "aurora" in variant or variant.startswith("ae"):
+            if "aurora" in variant :#or variant.startswith("ae"):
                 continue
             variant_name = variant if not variant.startswith("ae") else "ae"
-            sns.lineplot(data["stoch"], data["PV"], estimator="mean", ci="sd", label=f"{member}-{variant_name}", ax=ax1,
+            sns.lineplot(data["stoch"], data["PV"], estimator=np.median, ci=None, label=f"{member}-{variant_name}", ax=ax1,
                          color=colours[colour_count])
             if i == 0 and len(group) > 1:
                 ax1.lines[-1].set_linestyle("--")
+            data_stats = pd.DataFrame(data)[["stoch", "PV"]].groupby("stoch").describe()
+            quart25 = data_stats[('PV', '25%')]
+            quart75 = data_stats[('PV', '75%')]
+            ax1.fill_between([0, 1, 2, 3, 4, 5], quart25, quart75, alpha=0.3, color=colours[colour_count])
+
             colour_count += 1
     ax1.set_title("Variance of Trajectory Positions")
-    ax1.set_ylabel("Mean Variance")
+    ax1.set_ylabel("Variance")
     ax1.set_xlabel("Stochasticity")
     plt.savefig(f"{save_dir}/pdf/posvar_{'_'.join(group)}.pdf")
     plt.savefig(f"{save_dir}/posvar_{'_'.join(group)}.png")
@@ -224,16 +260,20 @@ for group in plotting_groups:
             log_data = pk.load(f)
 
         for variant, data in log_data.items():
-            if "aurora" in variant or variant.startswith("ae"):
+            if "aurora" in variant :#or variant.startswith("ae"):
                 continue
             variant_name = variant if not variant.startswith("ae") else "ae"
-            sns.lineplot(data["stoch"], data["NMV"], estimator="mean", ci="sd", label=f"{member}-{variant_name}", ax=ax1,
+            sns.lineplot(data["stoch"], data["NMV"], estimator=np.median, ci=None, label=f"{member}-{variant_name}", ax=ax1,
                          color=colours[colour_count])
+            data_stats = pd.DataFrame(data)[["stoch", "NMV"]].groupby("stoch").describe()
+            quart25 = data_stats[('NMV', '25%')]
+            quart75 = data_stats[('NMV', '75%')]
+            ax1.fill_between([0, 1, 2, 3, 4, 5], quart25, quart75, alpha=0.3, color=colours[colour_count])
             if i == 0 and len(group) > 1:
                 ax1.lines[-1].set_linestyle("--")
             colour_count += 1
-    ax1.set_title("Reconstruction Var. of No-Move solutions")
-    ax1.set_ylabel("Mean Reconstruction Variance")
+    ax1.set_title("Var. of Constructions of No-Move solutions")
+    ax1.set_ylabel("Variance")
     ax1.set_xlabel("Stochasticity")
     plt.savefig(f"{save_dir}/pdf/recon_var_{'_'.join(group)}.pdf")
     plt.savefig(f"{save_dir}/recon_var_{'_'.join(group)}.png")
@@ -248,15 +288,19 @@ for group in plotting_groups:
             log_data = pk.load(f)
 
         for variant, data in log_data.items():
-            if "aurora" in variant or variant.startswith("ae"):
+            if "aurora" in variant :#or variant.startswith("ae"):
                 continue
             variant_name = variant if not variant.startswith("ae") else "ae"
-            sns.lineplot(data["stoch"], data["LV"], estimator="mean", ci="sd", label=f"{member}-{variant_name}", ax=ax1,
+            sns.lineplot(data["stoch"], data["LV"], estimator=np.median, ci=None, label=f"{member}-{variant_name}", ax=ax1,
                          color=colours[colour_count])
+            data_stats = pd.DataFrame(data)[["stoch", "LV"]].groupby("stoch").describe()
+            quart25 = data_stats[('LV', '25%')]
+            quart75 = data_stats[('LV', '75%')]
+            ax1.fill_between([0, 1, 2, 3, 4, 5], quart25, quart75, alpha=0.3, color=colours[colour_count])
             if i == 0 and len(group) > 1:
                 ax1.lines[-1].set_linestyle("--")
             colour_count += 1
-    ax1.set_ylabel("Mean Variance")
+    ax1.set_ylabel("Variance")
     ax1.set_xlabel("Stochasticity")
     ax1.set_title(f"Variance of Latent Descriptors of No-Move Solutions")
     plt.savefig(f"{save_dir}/pdf/latent_var_{'_'.join(group)}.pdf")
@@ -274,12 +318,18 @@ for group in plotting_groups:
         for variant, data in log_data.items():
             if "aurora" in variant or variant.startswith("ae"):
                 continue
-            sns.lineplot(data["stoch"], data["ENVAR"] / 2, estimator="mean", ci="sd", label=f"{member}-{variant}", ax=ax1,
+            sns.lineplot(data["stoch"], data["ENVAR"] / 2, estimator=np.median, ci=None, label=f"{member}-{variant}", ax=ax1,
                          color=colours[colour_count])
+            data = pd.DataFrame(data)[["stoch", "ENVAR"]]
+            data["ENVAR"] /= 2
+            data_stats = data.groupby("stoch").describe()
+            quart25 = data_stats[('ENVAR', '25%')]
+            quart75 = data_stats[('ENVAR', '75%')]
+            ax1.fill_between([0, 1, 2, 3, 4, 5], quart25, quart75, alpha=0.3, color=colours[colour_count])
             if i == 0 and len(group) > 1:
                 ax1.lines[-1].set_linestyle("--")
             colour_count += 1
-    ax1.set_ylabel("Mean Variance")
+    ax1.set_ylabel("Variance")
     ax1.set_xlabel("Stochasticity")
     ax1.set_title(f"Encoder Variance")
     plt.savefig(f"{save_dir}/pdf/encoder_var_{'_'.join(group)}.pdf")
