1ca8b81b306256bc2e864ed62a2e0ff12879b70f
Andy Wang <aw1912@ic.ac.uk>
2020-08-11 11:44:51 +0100

    deleting empty keys from dict

--- python/visualisation/plot_variants.py
+++ python/visualisation/plot_variants.py
@@ -9,12 +9,17 @@ path = "/media/andwang1/SAMSUNG/MSC_INDIV/results_box2d_bsd_exp1"
 os.chdir(path)
 
 plotting_groups = [
-    # ["tsne_nosampletrain_beta0", "tsne_nosampletrain"],
+    # ["l2withsampling", "l2withsampling_beta0"],
+    # ["l2", "l2withsampling"],
+    # ["l2", "l2beta0"],
+    # ["l2_nosampletrain", "l2"],
+    # ["l2_nosampletrain", "l2beta0_nosampletrain"],
 # ["tsne_nosampletrain_beta0", "sne_sampletrain", "sne_nosampletrain", "tsne_nosampletrain"],
 # ["l1beta0nosample","l1nosample", "l1beta0"],
 # ["l2beta0nosample","l2nosample", "l2beta0"],
 #     ["sne_nosampletrain_beta0", "sne_nosampletrain"]
-["l2nosampletrain", "sne_nosampletrain", "tsne_nosampletrain"]
+#     ["l2nosampletrain_add5", "l2nosampletrain"],
+["l2nosampletrain", "sne_nosampletrain_beta0", "tsne_nosampletrain_beta0"]
 # ["sne_nosampletrain_beta1", "tsne_nosampletrain_beta1", "l2beta1nosampletrain"],
 # ["l2beta0nosampletrain", "l2beta1nosampletrain"],
 # ["l2beta1nosampletrain", "l2"], #compare against ae
@@ -71,6 +76,9 @@ for group in plotting_groups:
             sns.lineplot(data["stoch"], data["AL"], estimator=np.median, ci=None, label=f"{member}-{variant_name}", ax=ax1, color=colours[colour_count])
             if i == 0 and len(group) > 1:
                 ax1.lines[-1].set_linestyle("--")
+            empty_keys = [k for k in data if not len(data[k])]
+            for k in empty_keys:
+                del data[k]
             data_stats = pd.DataFrame(data)[["stoch", "AL"]].groupby("stoch").describe()
             quart25 = data_stats[('AL', '25%')]
             quart75 = data_stats[('AL', '75%')]
