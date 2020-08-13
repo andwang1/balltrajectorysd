b9d97219e991f238cc76871c40fc507770bcc667
Andy Wang <aw1912@ic.ac.uk>
2020-08-13 16:40:33 +0100

    adding tool to plot trajectories across PIDs

---
+++ python/visualisation/visualise_experiment_trajectories.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot as plt
+import time
+import os
+import numpy as np
+from exp_config import *
+
+GEN_NUMBER = 6000
+FILE_NAME = f'undist_traj_{GEN_NUMBER}.dat'
+BUCKET_FILE_NAME = f'distances{GEN_NUMBER}.dat'
+BUCKET_IDX = 102
+
+FULL_PATH = "/home/andwang1/airl/DISTPLOTS/fordevelopingarchivevisualisation/results_balltrajectorysd_vae/gen6001_random1_fulllosstrue_beta0_extension0_lossfunc2_samplefalse_tsne1"
+
+PIDs = [name for name in os.listdir(FULL_PATH) if os.path.isdir(os.path.join(FULL_PATH, name))]
+
+for dir in PIDs:
+    os.chdir(FULL_PATH)
+    FILE = FULL_PATH + "/" + dir +  "/" + FILE_NAME
+    BUCKET_FILE = FULL_PATH + "/" + dir +  "/" + BUCKET_FILE_NAME
+    with open(FILE, 'r') as f:
+        lines = f.readlines()
+
+    # list of lists of recon, trajectories, losses
+    plotting_data = []
+
+    for line in lines:
+        plotting_data.append([float(i.strip()) for i in line.split(",")])
+
+    with open(BUCKET_FILE, "r") as f:
+        lines = f.readlines()
+    for line in lines:
+        data = line.split(",")
+        if data[0].strip() != str(BUCKET_IDX):
+            continue
+        indices = [int(i) for i in data[1:]]
+        break
+    print(indices)
+    plotting_data = [data for i, data in enumerate(plotting_data) if i in indices]
+
+    f = plt.figure(figsize=(5, 5))
+    spec = f.add_gridspec(1, 1)
+    # both kwargs together make the box squared
+    ax1 = f.add_subplot(spec[0, 0], aspect='equal', adjustable='box')
+    ax1.set_ylim([ROOM_H, 0])
+    ax1.set_xlim([0, ROOM_W])
+    ax1.set_title("All Trajectories")
+
+    for idx, indiv in enumerate(plotting_data):
+        if idx % 100 == 0:
+            print(f"Processing {idx}th trajectory")
+        # The data
+        x_label = indiv[::2]
+        y_label = indiv[1::2]
+
+        # Plot
+        ax1.plot(x_label, y_label, color="gray", alpha=0.01)
+plt.show()
+# plt.savefig("sne_nst_b0_rand0_visualisation.pdf")
